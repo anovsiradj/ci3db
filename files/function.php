7ad02b3a1b683fcb784b6b9375a37871f0df9d7f
anovsiradj <anov.siradj22@gmail.com>
2017-10-16 11:09:03 +0700

    files/function - add remove invisible chars

--- files/function.php
+++ files/function.php
@@ -1,18 +1,50 @@
 <?php
-// to satisfy only
-if (!function_exists('log_message')) {
+/**
+* to satisfy only
+* 
+*/
+if (function_exists('log_message') === false) {
 	function log_message() {}
 }
 
-function &load_class($class)
-{
-	static $instances = array();
+/**
+* Required.
+* If you wanted to use default remove_invisible_characters() function,
+* you can load system/core/Common.php
+* 
+* example: require BASEPATH . 'core/Common.php';
+* 
+* @see system/database/DB_driver.php - CI_DB_driver:_escape_str()
+* 
+*/
+if (function_exists('remove_invisible_characters') === false) {
+	function remove_invisible_characters($str) { return $str; }
+}
 
-	$instance = 'anovsiradj\\CI3DB\\' . $class;
+/**
+* Important Required.
+* 
+* To load fake CI3 classes
+* 
+* @see ../class/
+* 
+*/
+if (function_exists('load_class')) {
+	throw new Exception('I am sorry. But this package require load_class() function.');
+} else {
+	function &load_class($class) {
+		static $instances = array();
 
-	// basically, this is to trigger php to run autoloader to load/register requested class if it not exist
-	class_exists($instance, true);
+		$instance = '\\anovsiradj\\CI3DB\\' . $class;
 
-	$instances[$instance] = $instance::init();
-	return $instances[$instance];
+		if (isset($instances[$instance]) === false) {
+
+			// basically, this is to trigger php to run autoloader to load/register class if it not exist
+			class_exists($instance, true);
+
+			$instances[$instance] = &$instance::init();
+		}
+
+		return $instances[$instance];
+	}
 }
