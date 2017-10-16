4ee30659d49a937f022ed837de6569265ac45f85
anovsiradj <anov.siradj22@gmail.com>
2017-10-16 10:36:38 +0700

    class/lang - make it more overridable

--- class/Lang.php
+++ class/Lang.php
@@ -1,33 +1,77 @@
 <?php
+/**
+* CI3 Fake Lang(uage) Class
+* 
+* But you still can using it, with some extra config.
+* example:
+* $lang =& anovsiradj\CI3DB\Lang::init();
+* $lang::$default = 'indonesia';
+* $lang::$path = 'myapp/external/ci3/'; // mean myapp/external/ci3/language/indonesia/db_lang.php
+* $lang->reload();
+* 
+* Or if you have a better idea, please contact me at this github repository.
+* 
+* @see system/core/Lang.php
+* 
+*/
+
 namespace anovsiradj\CI3DB;
 
 class Lang
 {
 	protected static $self_instance;
-	protected $lang;
+	protected $lang = array();
+
+	public static $default = 'english';
+	public static $path = null;
 
 	protected function __construct()
 	{
-		include BASEPATH . 'language/english/db_lang.php';
+		$this->reload();
+	}
 
-		if (!isset($lang)) $lang = array();
+	/**
+	* @see https://github.com/bcit-ci/codeigniter3-translations
+	* for more info and language format file.
+	* 
+	*/
+	public function reload()
+	{
+		$path = BASEPATH;
+		if (static::$path !== null) $path = ltrim(static::$path, '/') . '/';
 
-		foreach ($lang as $k => $v) {
-			$this->lang[$k] = $v;
+		$file = $path . 'language/' . static::$default . '/db_lang.php';
+
+		if (file_exists($file)) {
+			include $file;
+
+			if (isset($lang) === false) $lang = array();
+
+			foreach ($lang as $k => $v) {
+				$this->lang[$k] = $v;
+			}
 		}
 	}
 
 	public static function &init()
 	{
-		if (!isset(static::$self_instance)) {
-			static::$self_instance = new Lang();
+		if (isset(static::$self_instance) === false) {
+			static::$self_instance = new self;
 		}
+
 		return static::$self_instance;
 	}
 
-	// to satisfy only
+	/**
+	* to satisfy only
+	* 
+	*/
 	public function load() {}
 
+	/**
+	* simplified version
+	* 
+	*/
 	public function line($k) {
 		if (isset($this->lang[$k])) return $this->lang[$k];
 		return $k;
