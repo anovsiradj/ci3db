b5022cdd0223b61f3c51dd524c897b8584cac182
anovsiradj <anov.siradj22@gmail.com>
2017-10-16 10:34:01 +0700

    update class/exceptions - make error handler dynamic and verbose

--- class/Exceptions.php
+++ class/Exceptions.php
@@ -1,4 +1,11 @@
 <?php
+/**
+* CI3 Fake Exception(s) Class
+* 
+* @see system/core/Exceptions.php
+* 
+*/
+
 namespace anovsiradj\CI3DB;
 use Exception;
 
@@ -11,12 +18,39 @@ class Exceptions
 	public static function &init()
 	{
 		if (!isset(static::$self_instance)) {
-			static::$self_instance = new Exceptions();
+			static::$self_instance = new self;
 		}
 		return static::$self_instance;
 	}
 
-	// system/core/Exceptions.php
+	/**
+	* All error function, will trought this.
+	* I am too lazy to make every error function.
+	* 
+	*/
+	protected function __default()
+	{
+		$args = func_get_args();
+		$args[0] = '@' . $args[0] . '():'; // error caller identifier
+
+		$msgs = '';
+		foreach ($args as $arg) {
+			if (is_array($arg)) {
+				$msgs .= implode(PHP_EOL, $arg) . PHP_EOL;
+			} else {
+				$msgs .= $arg . PHP_EOL;
+			}
+		}
+		throw new Exception($msgs);
+	}
+
+	public function __call($fn, $args)
+	{
+		array_unshift($args, $fn);
+		call_user_func_array(array($this, '__default'), $args);
+	}
+
+	/*
 	public function show_error($heading, $message)
 	{
 		if (is_array($message)) {
@@ -24,5 +58,5 @@ class Exceptions
 		}
 		throw new Exception($heading . ':' . PHP_EOL . $message);
 	}
-
+	*/
 }
